d6c672e5c6b313f2edcea8c61be299115b301ed5
mahmoudali747 <mahmoud.ali2962@gmail.com>
2023-09-07 08:01:16 -0700

    haaaaaaaaaaaaaa

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
+    """
+    Handle basic arithmetic operations.
+    """
+    from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) - 1 != 3:
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python3
-
+#!/bin/usr/bin/ptyhon3
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
-
-    if a < b:
+    if (a < b):
         c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+        for z in range(4, 6):
+            c = add(c, z)
+        return(c)
+    return(sub(a, b))
